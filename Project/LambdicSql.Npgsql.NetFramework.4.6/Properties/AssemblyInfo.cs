14f93013b1b790349e24cba48dd63b5d69b23c45
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-07-16 06:48:40 +0900

    version up.

--- Project/LambdicSql.Npgsql.NetFramework.4.6/Properties/AssemblyInfo.cs
+++ Project/LambdicSql.Npgsql.NetFramework.4.6/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("4a87b37e-8b5f-4ec4-9509-48635c8d1905")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
