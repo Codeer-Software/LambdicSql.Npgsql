32e2b79fb1d14075a076903ebeaad3b83aeda86a
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2020-10-13 16:19:54 +0900

    Version up with dependency packages.

---
+++ Project/TestCore/TestAsync.cs
@@ -0,0 +1,77 @@
+﻿using System.Data;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LambdicSql;
+using LambdicSql.feat.Dapper;
+using static LambdicSql.Npgsql.Symbol;
+using static Test.Helper.DBProviderInfo;
+using Test.Helper;
+
+namespace Test
+{
+    [TestClass]
+    public class TestAsync
+    {
+        public TestContext TestContext { get; set; }
+        public IDbConnection _connection;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _connection = TestEnvironment.CreateConnection(TestContext);
+            _connection.Open();
+        }
+
+        [TestCleanup]
+        public void TestCleanup() => _connection.Dispose();
+
+        public class SelectData
+        {
+            public string Type { get; set; }
+        }
+
+        [TestMethod]
+        public void Test_QueryAsync()
+        {
+            var sql = Db<DB>.Sql(db =>
+                Select(new SelectData()
+                {
+                    Type = Case().
+                                When(db.tbl_staff.id == 3).Then("x").
+                                When(db.tbl_staff.id == 4).Then("y").
+                                Else("z").
+                            End()
+                }).
+                From(db.tbl_staff));
+
+            var datas = _connection.QueryAsync(sql).Result.ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(sql, _connection,
+@"SELECT
+	CASE
+		WHEN tbl_staff.id = @p_0
+		THEN @p_1
+		WHEN tbl_staff.id = @p_2
+		THEN @p_3
+		ELSE @p_4
+	END AS Type
+FROM tbl_staff",
+ 3, "x", 4, "y", "z");
+        }
+
+        [TestMethod]
+        public void Test_ExecuteAsync()
+        {
+            var sql = Db<DB>.Sql(db =>
+                   InsertInto(db.tbl_data, db.tbl_data.id, db.tbl_data.val2) +
+                   Values(1, "val2"));
+
+            Assert.AreEqual(1, _connection.ExecuteAsync(sql).Result);
+            AssertEx.AreEqual(sql, _connection,
+@"INSERT INTO tbl_data(id, val2)
+	VALUES(@p_0, @p_1)",
+1, "val2");
+        }
+
+    }
+}
